76c2bf4cb10bcf320a376e5d45fcc470a75831b9
Cole Bemis <colebemis@gmail.com>
2017-07-03 15:05:29 -0700

    feat: Add JavaScript library
    Add JavaScript library that includes an `icons` object, `toSvg` function and `replace` function.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ * @file Exposes `feather` object.
+ */
+
+import icons from '../dist/icons.json';
+import toSvg from './to-svg';
+import replace from './replace';
+
+module.exports = { icons, toSvg, replace };
