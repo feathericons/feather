704870bb124ea9954c5349b204bb1fa5d020d411
Cole Bemis <colebemis@gmail.com>
2017-07-03 13:49:11 -0700

    refactor: Remove website code from `master` branch

--- bin/build-zip.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# find and store current version number
-version=$(grep '"version"' package.json | cut -d '"' -f4)
-
-# compress 'icons' directory into feather-[version].zip
-zip -r feather.zip ./icons/
