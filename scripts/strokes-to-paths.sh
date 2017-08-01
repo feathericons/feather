f291a476736535cee66a98ce0c15e1c3cc623909
Mateus Andrade <mat.andrade@live.com>
2017-08-01 17:00:02 -0300

    fix: output fixed

--- scripts/strokes-to-paths.sh
+++ scripts/strokes-to-paths.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo $1
-sed -i .bkp 's/stroke-width="2"/stroke-width="1"/g' $1
+#sed -i .bkp 's/stroke-width="2"/stroke-width="1"/g' $1
 inkscape $1 --verb=EditSelectAll --verb=SelectionUnGroup --verb=StrokeToPath --verb=FileSave --verb=FileQuit
 find . -name "*.bkp" -type f -delete
