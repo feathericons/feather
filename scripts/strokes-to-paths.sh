1696cbb0b8a9e085e8bbd03a5d410c830093421f
Mateus Andrade <mat.andrade@live.com>
2017-08-02 11:01:55 -0300

    fix: ttf and classapp css added. Also converted all strokes to path

--- scripts/strokes-to-paths.sh
+++ scripts/strokes-to-paths.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo $1
-#sed -i .bkp 's/stroke-width="2"/stroke-width="1"/g' $1
+find . -name "*.DS_STORE" -type f -delete
 inkscape $1 --verb=EditSelectAll --verb=SelectionUnGroup --verb=StrokeToPath --verb=FileSave --verb=FileQuit
 find . -name "*.bkp" -type f -delete
