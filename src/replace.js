0cfac109766085be96c6c149ef8cd5e27e9c183a
Cole Bemis <colebemis@gmail.com>
2017-11-18 15:09:01 -0800

    refactor: Refactor and test replace()

--- src/replace.js
+++ src/replace.js
@@ -1,54 +1,60 @@
-/**
- * @file Implements `replace` function.
- */
-
 /* global document, DOMParser */
+import classnames from 'classnames/dedupe';
 
-import icons from '../dist/icons.json';
-import toSvg from './to-svg';
+import icons from './icons';
 
 /**
- * Replace all elements that have a `data-feather` attribute with SVG markup
+ * Replace all HTML elements that have a `data-feather` attribute with SVG markup
  * corresponding to the element's `data-feather` attribute value.
- * @param {Object} options
+ * @param {Object} attrs
  */
-export default function replace(options = {}) {
+function replace(attrs = {}) {
   if (typeof document === 'undefined') {
     throw new Error('`feather.replace()` only works in a browser environment.');
   }
 
   const elementsToReplace = document.querySelectorAll('[data-feather]');
 
-  Array.from(elementsToReplace).forEach(element => replaceElement(element, options));
+  Array.from(elementsToReplace).forEach(element =>
+    replaceElement(element, attrs),
+  );
 }
 
 /**
- * Replace single element with SVG markup
+ * Replace a single HTML element with SVG markup
  * corresponding to the element's `data-feather` attribute value.
- * @param {Element} element
- * @param {Object} options
+ * @param {HTMLElement} element
+ * @param {Object} attrs
  */
-function replaceElement(element, options) {
-  const key = element.getAttribute('data-feather');
+function replaceElement(element, attrs = {}) {
+  const elementAttrs = getAttrs(element);
+  const name = elementAttrs['data-feather'];
+  delete elementAttrs['data-feather'];
 
-  if (!key) {
-    throw new Error('The required `data-feather` attribute has no value.');
-  }
-
-  if (!icons[key]) {
-    throw new Error(`No icon matching '${key}'. See the complete list of icons at https://feathericons.com`);
-  }
-
-  const elementClassAttr = element.getAttribute('class') || '';
-  const elementIdAttr = element.getAttribute('id');
-  const classNames = (
-    options.class ? `${options.class} ${elementClassAttr}` : elementClassAttr
+  const svgString = icons[name].toSvg({
+    ...attrs,
+    ...elementAttrs,
+    ...{ class: classnames(attrs.class, elementAttrs.class) },
+  });
+  const svgDocument = new DOMParser().parseFromString(
+    svgString,
+    'image/svg+xml',
   );
-
-  const svgOptions = Object.assign({}, options, { class: classNames, id: elementIdAttr });
-  const svgString = toSvg(key, svgOptions);
-  const svgDocument = new DOMParser().parseFromString(svgString, 'image/svg+xml');
   const svgElement = svgDocument.querySelector('svg');
 
   element.parentNode.replaceChild(svgElement, element);
 }
+
+/**
+ * Get the attributes of an HTML element.
+ * @param {HTMLElement} element
+ * @returns {Object}
+ */
+function getAttrs(element) {
+  return Array.from(element.attributes).reduce((attrs, attr) => {
+    attrs[attr.name] = attr.value;
+    return attrs;
+  }, {});
+}
+
+export default replace;
