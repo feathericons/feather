90aa94a2f6b561b8469bfb860e76a0fa8215f098
Cole Bemis <colebemis@gmail.com>
2017-11-19 16:22:53 -0800

    fix: Remove unecessary whitespace from SVG contents
    Example:
    ```js
    /* BEFORE */
    { circle: "\n  <circle cx=\"12\" cy=\"12\" r=\"10\"></circle>\n" }
    
    /* AFTER */
    { circle: "<circle cx=\"12\" cy=\"12\" r=\"10\"></circle>" }
    ```

--- bin/__tests__/build-icons-object.test.js
+++ bin/__tests__/build-icons-object.test.js
@@ -3,9 +3,9 @@ import buildIconsObject from '../build-icons-object';
 
 const SVG_FILES = {
   'icon1.svg':
-    '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="23" y1="1" x2="1" y2="23" /><line x1="1" y1="1" x2="23" y2="23" /></svg>',
+    '<svg\n  xmlns="http://www.w3.org/2000/svg"\n  width="24"\n  height="24"\n  viewBox="0 0 24 24"\n>\n  <line x1="23" y1="1" x2="1" y2="23" />\n  <line x1="1" y1="1" x2="23" y2="23" />\n</svg>',
   'icon2.svg':
-    '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="11" /></svg>',
+    '<svg\n  xmlns="http://www.w3.org/2000/svg"\n  width="24"\n  height="24"\n  viewBox="0 0 24 24"\n>\n  <circle cx="12" cy="12" r="11" />\n</svg>',
 };
 
 function getSvg(svgFile) {
