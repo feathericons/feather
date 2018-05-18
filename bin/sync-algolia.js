1d3283d81db2b51bc758261e6f3431a1db772786
Cole Bemis <colebemis@gmail.com>
2018-05-17 21:51:49 -0700

    build: Add sync-algolia script

---
+++ bin/sync-algolia.js
@@ -0,0 +1,34 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import algolia from 'algoliasearch';
+import icons from '../dist/icons.json';
+import tags from '../src/tags.json';
+
+const ALGOLIA_APP_ID = '5EEOG744D0';
+
+if (
+  process.env.TRAVIS_PULL_REQUEST === 'false' &&
+  process.env.TRAVIS_BRANCH === 'master'
+) {
+  syncAlgolia();
+}
+
+function syncAlgolia() {
+  const client = algolia(ALGOLIA_APP_ID, process.env.ALGOLIA_ADMIN_KEY);
+
+  const index = client.initIndex('icons');
+
+  const records = Object.keys(icons).map(name => ({
+    name,
+    tags: tags[name] || [],
+  }));
+
+  index.clearIndex((err, content) => {
+    if (err) throw err;
+    console.log(content); // eslint-disable-line no-console
+  });
+
+  index.addObjects(records, (err, content) => {
+    if (err) throw err;
+    console.log(content); // eslint-disable-line no-console
+  });
+}
