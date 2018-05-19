ae8e149850017796606d64a5d8ae6321189df62d
Cole Bemis <colebemis@gmail.com>
2018-05-18 17:23:23 -0700

    build: Refactor algolia script to reindex data atomically

--- bin/sync-algolia.js
+++ bin/sync-algolia.js
@@ -18,20 +18,32 @@ function syncAlgolia() {
   // ALGOLIA_ADMIN_KEY must be added as an environment variable in Travis CI
   const client = algolia(ALGOLIA_APP_ID, process.env.ALGOLIA_ADMIN_KEY);
 
+  // Initialize the target and temporary indexes
   const index = client.initIndex('icons');
+  const indexTmp = client.initIndex('icons_tmp');
 
+  // Copy the settings, synonyms and rules (but not the records)
+  // of the target index into the temporary index
+  client.copyIndex(index.indexName, indexTmp.indexName, [
+    'settings',
+    'synonyms',
+    'rules',
+  ]);
+
+  // Push data to the temporary index
   const records = Object.keys(icons).map(name => ({
     name,
     tags: tags[name] || [],
   }));
 
-  index.clearIndex((err, content) => {
+  indexTmp.addObjects(records, (err, content) => {
     if (err) throw err;
     console.log(content);
+  });
 
-    index.addObjects(records, (err, content) => {
-      if (err) throw err;
-      console.log(content);
-    });
+  // Move the temporary index to the target index
+  client.moveIndex(indexTmp.indexName, index.indexName, (err, content) => {
+    if (err) throw err;
+    console.log(content);
   });
 }
