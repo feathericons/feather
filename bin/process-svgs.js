7708bf8a16a12ee1205f280af0eb633b94f8dd7f
toir <toir427@gmail.com>
2021-03-15 23:23:40 +0500

    refactoring

--- bin/process-svgs.js
+++ bin/process-svgs.js
@@ -7,8 +7,7 @@ const IN_DIR = path.resolve(__dirname, '../icons');
 
 console.log(`Processing SVGs in ${IN_DIR}...`);
 
-fs
-  .readdirSync(IN_DIR)
+fs.readdirSync(IN_DIR)
   .filter(file => path.extname(file) === '.svg')
   .forEach(svgFile => {
     const svg = fs.readFileSync(path.join(IN_DIR, svgFile));
