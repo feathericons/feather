2cc6d674a191246fa6694d22c0546ea59499ba69
Cole Bemis <colebemis@github.com>
2023-08-19 22:04:32 -0700

    Install tailwind

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()]
+});
