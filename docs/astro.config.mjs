af89f6b746428e97665d6b94d8ebd8777f1dbc41
Cole Bemis <colebemis@github.com>
2023-08-19 22:12:27 -0700

    Configure react

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), react()],
 });
